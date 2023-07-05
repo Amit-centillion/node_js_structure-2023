210da16d21bb01095373594b0341b759edd574e9
Amit-centillion <108717420+Amit-centillion@users.noreply.github.com>
2023-07-05 19:23:20 +0530

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
-import express from 'express';
+const express = require("express");
+const test = require("./test-api.route");
 
 const router = express.Router();
-
-router.use('/test',require('./test-api.route'))
+router.use("/test", test);
 
 module.exports = router;
